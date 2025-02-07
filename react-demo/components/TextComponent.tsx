2d635d9b81888e6faff468e55d28e4212c3b3c03
Mahmut <miftarosky@hotmail.com>
2025-02-08 00:34:03 +0100

    feat: added TextComponent

---
+++ react-demo/components/TextComponent.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+export default function TextComponent() {
+    return (
+        <View>
+            <Text>Test Mahmut</Text>
+        </View>
+    )
+}
