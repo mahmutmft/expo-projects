f74be527a6cc6f551142088cd96dc5a3a3bf3cee
Mahmut <miftarosky@hotmail.com>
2025-02-08 00:34:38 +0100

    feat: added ButtonComponent

---
+++ react-demo/components/ButtonCompomemt.tsx
@@ -0,0 +1,11 @@
+import {View, Text, Button} from 'react-native'
+import React from 'react'
+
+export default function ButtonCompomemt() {
+    return (
+        <View>
+            <Text>ButtonCompomemt</Text>
+            <Button title='Press Me' color="green"/>
+        </View>
+    )
+}
