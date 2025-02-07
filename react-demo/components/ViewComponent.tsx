9421a68b3ef564be41204a8d10415d45e4c69871
Mahmut <miftarosky@hotmail.com>
2025-02-08 00:33:23 +0100

    feat: added ViewComponent

---
+++ react-demo/components/ViewComponent.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native'
+import React, {Component} from 'react'
+
+export default class ViewComponent extends Component {
+    render() {
+        return (
+            <View>
+                <Text>ViewComponent</Text>
+            </View>
+        )
+    }
+}
